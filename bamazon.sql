8cdeaf0282af698f47f89759a0eef94efe845103
Kevin Perez <kevinperez996@outlook.com>
2019-10-28 17:28:09 -0400

    Populated Database table and created logic to read data inside the tables.

--- bamazon.sql
+++ bamazon.sql
@@ -15,18 +15,18 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10),
-("vanilla", "Chock", 100.00, 10);
+VALUES ("Iphone 11", "Tech", 850.99, 10),
+("Samsung Galaxy", "Tech", 730.00, 9),
+("Xbox one X", "Tech", 400.00, 12),
+("LG 4k TV", "Tech", 1950.50, 6),
+("Macbook Pro", "Tech", 1400.00, 13),
+("The Office T-Shirt", "Clothing", 19.99, 10),
+("Addidas Sweatshirt", "Clothing", 49.95, 9),
+("Levi's Jeans", "Clothing", 59.00, 16),
+("NorthFace Jacket", "Clothing", 150.30, 5),
+("Polo Hat", "Clothing", 40.00, 3),
+("Yeezy Boost 350", "Shoes", 200.00, 2),
+("Timberland Boots", "Shoes", 100.00, 10),
+("Nike Arimax 95", "Shoes", 180.00, 5),
+("Vans clasics", "Shoes", 90.50, 10),
+("Ugg Boots", "Shoes", 120.00, 1);
